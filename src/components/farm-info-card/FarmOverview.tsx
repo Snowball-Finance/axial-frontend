9e87f01decfd8307b061484f94d1ccd173abfd42
mohsen <mhsnprvr4@gmail.com>
2021-11-06 21:26:28 +0330

    brought back FarmOverview

--- src/components/farm-info-card/FarmOverview.tsx
+++ src/components/farm-info-card/FarmOverview.tsx
@@ -1,96 +1,114 @@
 import "./FarmOverview.scss"
 
+import {
+  AXIAL_MASTERCHEF_CONTRACT_ADDRESS,
+  POOLS_MAP,
+  PoolTypes,
+  TOKENS_MAP,
+} from "../../constants"
 import Button, { MdButton } from "../button/Button"
+import { PoolDataType, UserShareType } from "../../hooks/usePoolData"
 import React, { ReactElement } from "react"
+import { formatBNToShortString, formatBNToString } from "../../libs"
 import { Link } from "react-router-dom"
+import { Zero } from "@ethersproject/constants"
 import classNames from "classnames"
-import { BigNumber } from "ethers"
-
+import { ethers } from "ethers"
+import masterchef from "../../constants/abis/masterchef.json"
+import { useActiveWeb3React } from "../../hooks"
 import { useTranslation } from "react-i18next"
 
 interface Props {
-  feeAPR: string,
-  totalAPR: string,
-  tvl: string,
-  myTVL: string,
-  farmName: string,
-  shouldMigrate: boolean,
-  isOutdated: boolean,
-  isPaused: boolean,
-  hasShare: boolean,
-  userBalanceUSD: string,
-  farmRoute: string,
-  onClickMigrate: () => void,
-  reserve: string,
-  tokens: {
-    symbol: string;
-    name: string;
-    icon: string;
-    value: string;
-  }[]
-
+  poolRoute: string
+  poolData: PoolDataType
+  userShareData: UserShareType | null
+  onClickMigrate?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
+
 export default function FarmOverview({
-  feeAPR,
-  totalAPR,
-  tvl,
-  myTVL,
-  farmName,
-  shouldMigrate,
-  isPaused,
-  isOutdated,
-  hasShare,
-  userBalanceUSD,
-  tokens,
-  farmRoute,
+  poolData,
+  poolRoute,
+  userShareData,
   onClickMigrate,
 }: Props): ReactElement | null {
   const { t } = useTranslation()
+  const { type: poolType, isOutdated, lpToken } = POOLS_MAP[poolData.name]
+  const formattedDecimals = poolType === PoolTypes.USD ? 2 : 4
+  const shouldMigrate = !!onClickMigrate
+  const { library } = useActiveWeb3React()
+  const formattedData = {
+    name: poolData.name,
+    reserve: poolData.reserve
+      ? formatBNToShortString(poolData.reserve, 18)
+      : "-",
+    apy: poolData.apy
+      ? `${poolData.apy.toLocaleString(undefined, {
+        maximumFractionDigits: 2,
+      })}%`
+      : "-",
+    volume: poolData.volume ? `$${poolData.volume}` : "-",
+    userBalanceUSD: formatBNToShortString(
+      userShareData?.usdBalance || Zero,
+      18,
+    ),
+    tokens: poolData.tokens.map((coin) => {
+      const token = TOKENS_MAP[coin.symbol]
+      return {
+        symbol: token.symbol,
+        name: token.name,
+        icon: token.icon,
+        value: formatBNToString(coin.value, token.decimals, formattedDecimals),
+      }
+    }),
+  }
+  const hasShare = !!userShareData?.usdBalance.gt("0")
 
+  const masterchefContract = new ethers.Contract(
+    AXIAL_MASTERCHEF_CONTRACT_ADDRESS[43114],
+    masterchef,
+    library,
+  )
 
-
-  const onClickWithdraw = () => {
+  async function onClickWithdraw() {
     console.log("withdraw")
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    // await masterchefContract.withdrawAll()
+    await masterchefContract.withdrawAll()
   }
 
-  const onClickDeposit = () => {
+  async function onClickDeposit() {
     console.log("deposit")
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    // await masterchefContract.depositAll()
+    await masterchefContract.depositAll()
   }
 
-  const onClickClaim = () => {
+  async function onClickClaim() {
     console.log("claim")
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    // await masterchefContract.claim()
+    await masterchefContract.claim()
   }
-
+  console.log({ tokens: formattedData.tokens })
   return (
     <div
-      className={classNames("poolOverview",
-        // {
-        //   outdated: isOutdated || shouldMigrate,
-        // }
-      )}
+      className={classNames("poolOverview", {
+        outdated: isOutdated || shouldMigrate,
+      })}
     >
       <div className="left">
         <div className="titleAndTag">
-          <h4 className="title">{farmName}</h4>
+          <h4 className="title">{formattedData.name}</h4>
           {(shouldMigrate || isOutdated) && <Tag kind="warning">OUTDATED</Tag>}
-          {isPaused && <Tag kind="error">PAUSED</Tag>}
+          {poolData.isPaused && <Tag kind="error">PAUSED</Tag>}
         </div>
         {hasShare && (
           <div className="balance">
             <span>{t("balance")}: </span>
-            <span>{`$${userBalanceUSD}`}</span>
+            <span>{`$${formattedData.userBalanceUSD}`}</span>
           </div>
         )}
         <div className="tokens">
           <span style={{ marginRight: "8px" }}>[</span>
-          {tokens.map(({ symbol, icon }) => (
+          {formattedData.tokens.map(({ symbol, icon }) => (
             <div className="token" key={symbol}>
               <img alt="icon" src={icon} />
               <span>{symbol}</span>
@@ -102,28 +120,25 @@ export default function FarmOverview({
 
       <div className="right">
         <div className="poolInfo">
-
-          <div className="margin">
-            <span className="label">fee APR</span>
-            <span>{feeAPR}</span>
-          </div>
-          <div className="margin">
-            <span className="label">total APR</span>
-            <span>{totalAPR}</span>
-          </div>
-
+          {formattedData.apy && (
+            <div className="margin">
+              <span className="label">{`${t("apy")}`}</span>
+              <span>{formattedData.apy}</span>
+            </div>
+          )}
           <div className="margin">
             <span className="label">TVL</span>
-            <span>{tvl}</span>
+            <span>{`$${formattedData.reserve}`}</span>
           </div>
-          {myTVL && <div className="margin">
-            <span className="label">my TVL</span>
-            <span>{myTVL}</span>
-          </div>}
-
+          {/*formattedData.volume && (
+            <div>
+              <span className="label">{`${t("24HrVolume")}`}</span>
+              <span>{formattedData.volume}</span>
+            </div>
+          )*/}
         </div>
         <div className="buttons">
-          <Link to={`${farmRoute}/withdraw`}>
+          <Link to={`${poolRoute}/withdraw`}>
             <Button kind="secondary">{t("withdraw")}</Button>
           </Link>
           {shouldMigrate ? (
@@ -135,17 +150,37 @@ export default function FarmOverview({
               {t("migrate")}
             </Button>
           ) : (
-            <Link to={`${farmRoute}/deposit`}>
+            <Link to={`${poolRoute}/deposit`}>
               <Button
                 kind="primary"
-                disabled={isPaused || isOutdated}
+                disabled={poolData?.isPaused || isOutdated}
               >
                 {t("deposit")}
               </Button>
             </Link>
           )}
         </div>
-
+        {/* <span style={{ marginTop: "8px" }}></span> */}
+        {/* <div className="poolInfo">
+          <span className="label">Rewards: </span>
+          <span style={{ marginLeft: "8px" }}></span>
+          <MdButton kind="temporary" onClick={onClickClaim}>
+            {t("claim")}
+          </MdButton>
+          <span style={{ marginLeft: "8px" }}></span>
+          <MdButton kind="secondary" onClick={onClickWithdraw}>
+            {t("withdraw")}
+          </MdButton>
+          <span style={{ marginLeft: "8px" }}></span>
+          <span style={{ marginTop: "8px" }}></span>
+          <MdButton
+            kind="primary"
+            onClick={onClickDeposit}
+            disabled={!hasShare}
+          >
+            {t("deposit")}
+          </MdButton>
+        </div> */}
       </div>
     </div>
   )
--- src/pages/farm/Farm.tsx
+++ src/pages/farm/Farm.tsx
@@ -39,9 +39,9 @@ function Farm(): ReactElement | null {
       <ul className={styles.filters}>
         {[
           ["all", "ALL"] as const,
-          // [PoolTypes.BTC, "BTC"] as const,
-          // [PoolTypes.ETH, "ETH"] as const,
-          // [PoolTypes.USD, "USD"] as const,
+          [PoolTypes.BTC, "BTC"] as const,
+          [PoolTypes.ETH, "ETH"] as const,
+          [PoolTypes.USD, "USD"] as const,
         ].map(([filterKey, text]) => (
           <li
             key={filterKey}
@@ -55,7 +55,52 @@ function Farm(): ReactElement | null {
         ))}
       </ul>
       <div className={styles.content}>
-        <FarmOverview
+        {Object.values(POOLS_MAP)
+          .filter(
+            ({ type, migration, isOutdated }) =>
+              filter === "all" ||
+              type === filter ||
+              (filter === "outdated" && (migration || isOutdated)),
+          )
+          .map(
+            ({ name, migration, isOutdated }) =>
+              [getPropsForPool(name), migration, isOutdated] as const,
+          )
+          .sort(
+            ([a, aMigration, aIsOutdated], [b, bMigration, bIsOutdated]) => {
+              // 1. active pools
+              // 2. user pools
+              // 3. higher TVL pools
+              if (aMigration || bMigration || aIsOutdated || bIsOutdated) {
+                return aMigration || aIsOutdated ? 1 : -1
+              } else if (
+                (a.userShareData?.usdBalance || Zero).gt(Zero) ||
+                (b.userShareData?.usdBalance || Zero).gt(Zero)
+              ) {
+                return (a.userShareData?.usdBalance || Zero).gt(
+                  b.userShareData?.usdBalance || Zero,
+                )
+                  ? -1
+                  : 1
+              } else {
+                return (a.poolData?.reserve || Zero).gt(
+                  b.poolData?.reserve || Zero,
+                )
+                  ? -1
+                  : 1
+              }
+            },
+          )
+          .map(([poolProps, migrationPool]) => (
+            <FarmOverview
+              key={poolProps.name}
+              {...poolProps}
+              onClickMigrate={
+                migrationPool ? () => handleClickMigrate() : undefined
+              }
+            />
+          ))}
+        {/* <FarmOverview
           farmName="A4D Stablecoins"
           farmRoute="/farm"
           feeAPR="10%"
@@ -99,7 +144,7 @@ function Farm(): ReactElement | null {
           ]}
 
 
-        />
+        /> */}
       </div>
       <Modal
         isOpen={!!currentModal}
