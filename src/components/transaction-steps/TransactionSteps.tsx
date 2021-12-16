06614dfca954af3d132524ee480f122deb8ca806
prodvdjin <pro.davidjin.dev@gmail.com>
2021-12-15 23:29:39 -0800

    updated withdraw steps in TransactionSteps component

--- src/components/transaction-steps/TransactionSteps.tsx
+++ src/components/transaction-steps/TransactionSteps.tsx
@@ -44,7 +44,6 @@ function TransactionSteps({ type, transactionData, transactionStatus }: Props): 
               <div key={key} className="stepItem">
                 <div className={transactionStatus?.approve?.[key] ? "dot checked": "dot unchecked"} />
                 <img src={POOLS_MAP[key].lpToken.icon} className="tokenIcon" alt='token-icon' />
-                <div>{POOLS_MAP[key].lpToken.symbol}</div>
               </div>
             ))}
           </div>
