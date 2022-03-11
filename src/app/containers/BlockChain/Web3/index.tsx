fe866ae161b5b9ffc701be2ebed2c96caaac9725
mohsen <mhsnprvr4@gmail.com>
2022-03-11 22:02:04 +0330

    same

--- src/app/containers/BlockChain/Web3/index.tsx
+++ src/app/containers/BlockChain/Web3/index.tsx
@@ -7,8 +7,13 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useWeb3Slice, Web3Actions } from "./slice";
-import { useWeb3React, Web3ReactProvider } from "@web3-react/core";
+import { createWeb3ReactRoot, useWeb3React, Web3ReactProvider } from "@web3-react/core";
+import { BaseProvider, getDefaultProvider } from "@ethersproject/providers"
+
 import { Web3Provider } from "@ethersproject/providers";
+import { NetworkContextName } from "../constants";
+import { rpcUrl } from "../utils/wallet/connectors";
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
 const Core = () => {
   useWeb3Slice();
@@ -52,16 +57,26 @@ const Core = () => {
   return <></>;
 };
 
+
+
 const getLibrary = (provider: any) => {
   const library = new Web3Provider(provider);
   library.pollingInterval = 8000;
   return library;
 };
 
+let networkLibrary: BaseProvider | undefined
+export function getNetworkLibrary(): BaseProvider {
+  const provider = getDefaultProvider(rpcUrl)
+  return (networkLibrary = networkLibrary ?? provider)
+}
+
 export const Web3 = () => {
   return (
     <Web3ReactProvider {...{ getLibrary }}>
+      <Web3ProviderNetwork getLibrary={getNetworkLibrary}>
       <Core />
+      </Web3ProviderNetwork>
     </Web3ReactProvider>
   );
 };
