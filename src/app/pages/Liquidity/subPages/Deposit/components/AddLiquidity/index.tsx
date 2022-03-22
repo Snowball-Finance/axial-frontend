306d259039b76564819070cfb2bfa18d2035ddad
Sanjeev Sharma <53838714+Sanjeev1308@users.noreply.github.com>
2022-03-23 04:46:11 +0530

    Worked on the liquidity deposit and withdraw modal design (#115)
    * Worked on the liquidity deposit and withdraw modal design
    
    * Added the functionality to open modals for testing purpose

--- src/app/pages/Liquidity/subPages/Deposit/components/AddLiquidity/index.tsx
+++ src/app/pages/Liquidity/subPages/Deposit/components/AddLiquidity/index.tsx
@@ -1,42 +1,58 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { styled, Grid, Typography } from "@mui/material";
 
 import { CssVariables } from "styles/cssVariables/cssVariables";
 import { ContainedButton } from "app/components/common/buttons/containedButton";
 import { AdvanceOption } from "./AdvanceOption";
 import { CurrencyInput } from "../../../../components/CurrencyInput";
+import { SnowModal } from "app/components/common/modal";
+import { DepositModal } from "app/pages/Liquidity/components/modal/Deposit";
 
 export const AddLiquidity: FC = () => {
+  const [open, setOpen] = useState<boolean>(false); //TODO: implemented for testing, please change the functionality while integration
+
   return (
-    <StyledAddLiquidity>
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          <HeaderText variant="h4">ADD LIQUIDITY</HeaderText>
-        </Grid>
+    <>
+      <StyledAddLiquidity>
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <HeaderText variant="h4">ADD LIQUIDITY</HeaderText>
+          </Grid>
 
-        {[1, 2, 3, 4].map((inputItem) => (
-          <Grid item key={inputItem}>
-            <Grid container direction="column" spacing={1}>
-              <Grid item alignSelf="end">
-                <BalanceText variant="body2">Wallet balance: 0.00</BalanceText>
-              </Grid>
+          {[1, 2, 3, 4].map((inputItem) => (
+            <Grid item key={inputItem}>
+              <Grid container direction="column" spacing={1}>
+                <Grid item alignSelf="end">
+                  <BalanceText variant="body2">
+                    Wallet balance: 0.00
+                  </BalanceText>
+                </Grid>
 
-              <Grid item>
-                <CurrencyInput />
+                <Grid item>
+                  <CurrencyInput />
+                </Grid>
               </Grid>
             </Grid>
+          ))}
+
+          <Grid item>
+            <AdvanceOption />
           </Grid>
-        ))}
 
-        <Grid item>
-          <AdvanceOption />
+          <Grid item alignSelf="center">
+            <ContainedButton width={220} onClick={() => setOpen(true)}>
+              Deposit
+            </ContainedButton>
+          </Grid>
         </Grid>
+      </StyledAddLiquidity>
 
-        <Grid item alignSelf="center">
-          <ContainedButton width={220}>Deposit</ContainedButton>
-        </Grid>
-      </Grid>
-    </StyledAddLiquidity>
+      {open && (
+        <SnowModal isOpen={open} onClose={() => setOpen(false)}>
+          <DepositModal />
+        </SnowModal>
+      )}
+    </>
   );
 };
 
