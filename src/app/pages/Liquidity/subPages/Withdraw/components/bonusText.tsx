9865341babb22217808696b212034d75f7431d89
mohsen <mhsnprvr4@gmail.com>
2022-04-01 02:18:33 +0430

    fixed bonus

--- src/app/pages/Liquidity/subPages/Withdraw/components/bonusText.tsx
+++ src/app/pages/Liquidity/subPages/Withdraw/components/bonusText.tsx
@@ -1,5 +1,5 @@
 import { RewardsSelectors } from "app/containers/Rewards/selectors";
-import { PoolData } from "app/containers/Rewards/types";
+import { PoolData, UserShareData } from "app/containers/Rewards/types";
 import { calculatePriceImpact } from "app/containers/Swap/utils/priceImpact";
 import { LiquidityPageSelectors } from "app/pages/Liquidity/selectors";
 import { BigNumber } from "ethers";
@@ -9,28 +9,21 @@ export const BonusText = () => {
   const pool = useSelector(LiquidityPageSelectors.selectedPool);
   const pools = useSelector(RewardsSelectors.pools);
   let poolData: PoolData | undefined = undefined;
+  let userShareData:UserShareData|undefined=undefined
+  let tokenInputSum=BigNumber.from(0)
   if (pool && pools[pool.key]) {
     poolData = pools[pool.key]?.poolData;
+    userShareData=pools[pool.key]?.userShareData
+    tokenInputSum =
+        userShareData?.masterchefBalance?.userInfo.amount ?? BigNumber.from("0")
   }
-  const withdrawAmounts = useSelector(
-    LiquidityPageSelectors.withdrawTokenAmounts
-  );
-  let sum = 0;
-  for (let k in withdrawAmounts) {
-    const v = withdrawAmounts[k];
-    if (Number(v) > 0) {
-      sum += Number(v);
-    }
-  }
-  const BNSum = BigNumber.from(sum.toString());
+
 
   let percentage = BigNumber.from(0);
   if (poolData) {
     percentage = calculatePriceImpact(
-      //ToDO: fix next line
-      // withdrawLPTokenAmount,
-      BigNumber.from(0),
-      BNSum,
+      tokenInputSum,
+      tokenInputSum,
       poolData.virtualPrice,
       true
     );
