cb3bdec9f0148ec2564aacbad250511abcce8125
mohsen <mhsnprvr4@gmail.com>
2022-06-03 22:24:57 +0430

    fixed pools

--- src/app/pools.ts
+++ src/app/pools.ts
@@ -24,7 +24,7 @@ export const pools =
           ],
           lpToken: tokens.SCALES,
           swapAddress: poolSwapAddress(Pools.SCALES),
-          gauge_address: "",
+          gauge_address: "0x63eD1dD9827a08CE2168139f8069edE751649D6c",
         },
         HERO: {
           key: Pools.HERO,
@@ -35,7 +35,7 @@ export const pools =
           poolTokens: [tokens.USDC, tokens.USDt, tokens.MIM, tokens.YUSD],
           lpToken: tokens.HERO,
           swapAddress: poolSwapAddress(Pools.HERO),
-          gauge_address: "",
+          gauge_address: "0x363A6fc27eD7a3dFDF94d33c37A6E8527c2014ea",
         },
         AS4D: {
           key: Pools.AS4D,
@@ -51,7 +51,7 @@ export const pools =
           ],
           lpToken: tokens.AS4D,
           swapAddress: poolSwapAddress(Pools.AS4D),
-          gauge_address: "",
+          gauge_address: "0xEE0a5Ce8C431B1253CA5545D7EfCA21fe63AD74F",
         },
         AC4D: {
           key: Pools.AC4D,
@@ -62,7 +62,7 @@ export const pools =
           poolTokens: [tokens.TSD, tokens.MIM, tokens.FRAX, tokens["DAI.e"]],
           lpToken: tokens.AC4D,
           swapAddress: poolSwapAddress(Pools.AC4D),
-          gauge_address: "",
+          gauge_address: "0xB81382a23ef5E5394cf993a5B16e0Dd7cf344D60",
         },
         AM3D: {
           key: Pools.AM3D,
@@ -73,6 +73,6 @@ export const pools =
           poolTokens: [tokens.MIM, tokens["USDC.e"], tokens["DAI.e"]],
           lpToken: tokens.AM3D,
           swapAddress: poolSwapAddress(Pools.AM3D),
-          gauge_address: "",
+          gauge_address: "0x321F5449bCf840297A027D92EA8723bc0a0388B5",
         },
       } as RewardsState["pools"]);
