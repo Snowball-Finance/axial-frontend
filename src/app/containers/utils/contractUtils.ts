007701994ccb9969f2d741e16831ec7b1dc6f26f
mohsen <mhsnprvr4@gmail.com>
2022-06-09 19:32:51 +0430

    fixed tvl on rewards page

--- src/app/containers/utils/contractUtils.ts
+++ src/app/containers/utils/contractUtils.ts
@@ -137,6 +137,26 @@ export function formatBNToShortString(
   return formattedData;
 }
 
+export const abbreviatedNumber = (num: number, digits = 2) => {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "m" },
+    { value: 1e9, symbol: "b" },
+    { value: 1e12, symbol: "t" },
+    { value: 1e15, symbol: "p" },
+    { value: 1e18, symbol: "e" },
+  ]
+  var item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value
+    })
+  return item ? (num / item.value).toFixed(digits) + item.symbol : "0"
+}
+
+
 export function commify(str: string): string {
   const parts = str.split(".");
   if (parts.length > 2)
