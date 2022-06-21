d1847da80d44e152979f932876c0e3479b1c59ff
mohsen <mhsnprvr4@gmail.com>
2022-06-21 21:39:23 +0430

    same

--- src/app/containers/BlockChain/Web3/constants.ts
+++ src/app/containers/BlockChain/Web3/constants.ts
@@ -1,13 +1,15 @@
-import { injected, walletLink } from "../utils/wallet/connectors";
+import { gnosisSafe, injected, walletLink } from "../utils/wallet/connectors";
 
 export const CONNECTORS = {
   MetaMask: injected,
   "Coinbase Wallet": walletLink,
   "Coin 98": injected,
+  "Gnosis Safe": gnosisSafe,
 };
 
 export const MOBILE_CONNECTORS = {
   MetaMask: injected,
   "Coinbase Wallet": walletLink,
   "Coin 98": injected,
+  "Gnosis Safe": gnosisSafe,
 };
